1781967f2c48215a8ec6f620997edfef3b20fbc5
JHXSMatthew <qilqil@live.com>
2018-12-03 18:32:55 +1100

    add EF dependency

---
+++ UserSystemApi/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace UserSystemApi.Models
+{
+    public class User
+    {
+        [Key]
+        public long UserID { get; set; }
+        [StringLength(16)]
+        [Index(IsUnique = true)]
+        public string UserName { get; set; }
+        //TODO: encoding
+        public string Password { get; set; }    
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+
+    }
+}
---
+++ UserSystemApi/Models/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UserSystemApi.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base()
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
